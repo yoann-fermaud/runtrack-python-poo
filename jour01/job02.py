7fe36d808060c47ad6f31ae15b486e3c7d2d5012
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-02-27 16:30:51 +0100

    [ADD] job02

---
+++ jour01/job02.py
@@ -0,0 +1,12 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 22
+        self.nombre2 = 12
+
+
+op = Operation()
+nombre_01 = op.nombre1
+nombre_02 = op.nombre2
+
+print(f"Le nombre1 est {nombre_01}")
+print(f"Le nombre2 est {nombre_02}")
